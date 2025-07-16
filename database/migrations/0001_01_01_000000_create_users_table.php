73a92d611407cb36eb2d1f65233c2f00765a6a3b
Felix <angelokipete@gmail.com>
2025-07-16 19:55:18 +0300

    refactor project deleted old Models and migrations to create new ones and Api packeges installed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->foreignId('user_role_id')->constrained('user_roles');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
